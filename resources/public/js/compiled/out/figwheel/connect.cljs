2b356a29a39fcbd7c29683d9e139c092b9848211
aleksanderesk <aleksanderesk@gmail.com>
2016-04-19 11:02:18 -0500

    compiled source

--- resources/public/js/compiled/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [turtle.core] [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:on-jsload (fn [& x] (if js/turtle.core.on-js-reload (apply js/turtle.core.on-js-reload x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'turtle.core/on-js-reload' is missing"))), :build-id "dev", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
